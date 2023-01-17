5c5864c7cf1fb254b856e2c5eb00ee21a6233e13
Iellen Luiza Lino Mendes <105090253+iellenLuiza@users.noreply.github.com>
2023-01-17 18:58:04 -0300

    gambiarra agente aceita

--- javascript.js
+++ javascript.js
@@ -1,9 +1,13 @@
-function espandir(id){
-    //var conteudos = document.getElementsByClassName('divInfResu');
+//expandir ou ocultar resumo
+function expandir(id) {
+    // faz lista com todos elementos da class divInfResu
+    var resumos = document.getElementsByClassName('divInfResu');
 
+    // pecorre a lista da class
+    for (var i = 0; i < resumos.length; i++) {
+        resumos[i].style.display = 'none';
+    }
+
+    // exibe elemento cuja o id foi passado
     document.getElementById(id).style.display = 'block';
-
-    /*for(var i = 0; i < conteudos.length; i++){
-        conteudos[i].style.display = 'none';
-    }*/
 }
