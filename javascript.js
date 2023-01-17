73cfe774f609765f3a3ab259fad8868354729c51
Iellen Luiza Lino Mendes <105090253+iellenLuiza@users.noreply.github.com>
2023-01-17 17:38:04 -0300

    ainda bugado

--- javascript.js
+++ javascript.js
@@ -1,28 +1,9 @@
-//// esconder e mostrar divs dos resumos 
-// pega as linhas da lista
-var itens = document.querySelectorAll("#resuminhos li"),
-    tab = [], index;
+function espandir(id){
+    //var conteudos = document.getElementsByClassName('divInfResu');
 
-// cria ação de clicar em um item específio
-for (var i = 0; i < itens.length; i++) {
+    document.getElementById(id).style.display = 'block';
 
-    // ação do item clicado
-    itens[i].onclick = function () {
-
-        /*não funciona, não passa o número do item clicado para a função expandir*/
-
-        // teste "manual" que função expandir funciona
-        expandir(0);
-
-    };
-}
-
-function expandir(idInfResu) {
-    document.getElementById(idInfResu).style.display = 'block';
-    var divInfResu = document.getElementsByClassName('divInfResu');
-    for(var c = 0; c < divInfResu.length; c++){
-        if(c != idInfResu){
-            divInfResu[c].style.display = 'none'
-        } 
-    }
+    /*for(var i = 0; i < conteudos.length; i++){
+        conteudos[i].style.display = 'none';
+    }*/
 }
