52b3e3fb5e6ebd45edbf28f15c100b20946536f7
Iellen Luiza Lino Mendes <105090253+iellenLuiza@users.noreply.github.com>
2023-05-03 21:42:52 -0300

    bagunça
    tentando por bd e sistema de login

--- javascript.js
+++ javascript.js
@@ -2,12 +2,10 @@
 function expandir(id) {
     // faz lista com todos elementos da class divInfResu
     var resumos = document.getElementsByClassName('divInfResu');
-
     // pecorre a lista da class
     for (var i = 0; i < resumos.length; i++) {
         resumos[i].style.display = 'none';
     }
-
     // exibe elemento cuja o id foi passado
     document.getElementById(id).style.display = 'block';
 }
